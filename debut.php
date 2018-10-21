1ce0f1c92d2b6f82ca226c7e320ee4a2858d5c68
xaviergallezot <43139822+xaviergallezot@users.noreply.github.com>
2018-10-21 14:57:48 +0200

    DEBUT DE LA PAGES D4ACCEUIL

---
+++ debut.php
@@ -0,0 +1,25 @@
+<!Doctype HTML>
+<HTML>
+	<HEAD>
+		<TITLE>PARKING</TITLE>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<link rel="stylesheet" type="text/css" href="cssForum2.css" />
+	</HEAD>
+	<BODY>
+		<h1 align=center >Bienvenue sur l'acceuil de notre sites de reservation de place <h1>
+		<?php
+		<ul id="menu">
+			<li><a href="INSCRIPTION.php">Inscription</a></li>
+			<li><a href="connexion.php">Connexion</a></li>	
+			<li><a href="">Contact</a></li>
+
+		</ul>
+		?>
+		
+		
+		
+				
+		
+	</BODY>
+</HTML>
+
