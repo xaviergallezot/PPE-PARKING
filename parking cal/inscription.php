b781a2f91994df13c04383bd29f86b73dde82fb2
xaviergallezot <43139822+xaviergallezot@users.noreply.github.com>
2018-10-30 15:14:03 +0100

    changement affichage

--- parking cal/inscription.php
+++ parking cal/inscription.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=parking', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=parking', 'root', '');
 	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e)
 {
