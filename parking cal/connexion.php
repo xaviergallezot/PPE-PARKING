561ec01c75fb7bc2d39acdda1281e9fbfe66e2a7
xaviergallezot <43139822+xaviergallezot@users.noreply.github.com>
2018-11-01 11:30:10 +0100

    CSS

--- parking cal/connexion.php
+++ parking cal/connexion.php
@@ -11,16 +11,18 @@ try
 if(isset($_POST["pseudo"]) && isset($_POST["motdepasse"]))
 {
 	$query = $bdd->prepare('SELECT motdepasse FROM membres WHERE pseudo = :pseudo');
+	echo $query ;
 	$query->bindParam(':pseudo', $_POST["pseudo"]);
 	$query->execute();
 	$result = $query->fetch();
 	$hash = $result[0];
-	
+	//$pseudobdd=$bdd->prepare ('SELECT pseudo FROM membres WHERE pseudo = :pseudo');
 	$correctPassword = password_verify($_POST["motdepasse"], $hash);
 	
 	if($correctPassword)
 	{
 		header('Location: utilisateur.html');
+		echo "ca marche"; 
 	}else{
 		header('Location: utilisateur.html');
 	}
